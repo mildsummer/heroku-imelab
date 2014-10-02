7ac6d381a2c655669e15ef387869af9f5e4953e1
mildsummer <mild.summer.y@gmail.com>
2014-10-02 22:49:45 +0900

    ect

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/:file', function(req, res) {
+  res.render(req.params.file);
+});
+
 module.exports = router;
