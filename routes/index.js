60c6fa982cb53f9c574d9fb1ea721c4c9f16035c
mildsummer <mild.summer.y@gmail.com>
2014-10-03 00:19:23 +0900

    ect

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', res.render('index'));
+router.get('/', function(req, res) {
+    res.render('index')
+});
 
-router.get('/template', res.render('index'));
+router.get('/template', function(req, res) {
+    res.render('index')
+});
 
 router.get('/:file', function(req, res) {
     res.render(req.params.file)
