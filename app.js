9cc9e37822aab5b5c3846f7c8a2e6633aabcb80a
mildsummer <mild.summer.y@gmail.com>
2014-10-02 21:54:51 +0900

    ect

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ var ECT = require('ect');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
 app.engine('ect', ECT({ watch: true, root: __dirname + '/views', ext: '.ect' }).render);
 app.set('view engine', 'ect');
 
